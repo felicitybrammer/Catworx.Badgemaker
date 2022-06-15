58a70e14e5b48f61ac466e1466a93f590c3cb3f1
Felicity Brammer <felicitybrammer@gmail.com>
2022-06-15 11:03:38 -0700

    first hello world

--- CatWorx.Badgemaker/Program.cs
+++ CatWorx.Badgemaker/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace CatWorx.Badgemaker 
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+
+
+
