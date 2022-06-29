7851d88761da9dbd3edd0edf1d6d3c442a13cd1a
Felicity Brammer <felicitybrammer@gmail.com>
2022-06-28 20:47:54 -0700

    lists and arrays practice

--- CatWorx.Badgemaker/Program.cs
+++ CatWorx.Badgemaker/Program.cs
@@ -42,22 +42,33 @@ namespace CatWorx.Badgemaker
             // Console.WriteLine(intNum);
             // Console.WriteLine(intNum.GetType());
 
-            Dictionary<string, int> myScoreBoard = new Dictionary<string, int>(){
-                { "firstInning", 10 },
-                { "secondInning", 20 },
-                { "thirdInning", 30 },
-                { "fourthInning", 40 },
-                { "fifthInning", 50 },
-            };
-            Console.WriteLine("----------------");
-            Console.WriteLine("  Scoreboard");
-            Console.WriteLine("----------------");
-            Console.WriteLine("Inning |  Score");
-            Console.WriteLine("   1   |    {0}", myScoreBoard["firstInning"]);
-            Console.WriteLine("   2   |    {0}", myScoreBoard["secondInning"]);
-            Console.WriteLine("   3   |    {0}", myScoreBoard["thirdInning"]);
-            Console.WriteLine("   4   |    {0}", myScoreBoard["fourthInning"]);
-            Console.WriteLine("   5   |    {0}", myScoreBoard["fifthInning"]);
+            // Dictionary<string, int> myScoreBoard = new Dictionary<string, int>(){
+            //     { "firstInning", 10 },
+            //     { "secondInning", 20 },
+            //     { "thirdInning", 30 },
+            //     { "fourthInning", 40 },
+            //     { "fifthInning", 50 },
+            // };
+            // Console.WriteLine("----------------");
+            // Console.WriteLine("  Scoreboard");
+            // Console.WriteLine("----------------");
+            // Console.WriteLine("Inning |  Score");
+            // Console.WriteLine("   1   |    {0}", myScoreBoard["firstInning"]);
+            // Console.WriteLine("   2   |    {0}", myScoreBoard["secondInning"]);
+            // Console.WriteLine("   3   |    {0}", myScoreBoard["thirdInning"]);
+            // Console.WriteLine("   4   |    {0}", myScoreBoard["fourthInning"]);
+            // Console.WriteLine("   5   |    {0}", myScoreBoard["fifthInning"]);
+
+            // string[] favFoods = new string[3]{ "pizza", "doughnuts", "icecream"};
+            // string firstFood = favFoods[0];
+            // string secondFood = favFoods[1];
+            // string thirdFood = favFoods[2];
+            // Console.WriteLine("I like {0}, {1}, and {2}", firstFood, secondFood, thirdFood);
+
+            List<string> employees = new List<string>() { "adam", "amy"};
+            employees.Add("barbara");
+            employees.Add("billy");
+            Console.WriteLine("My employees include {0}, {1}, {2}, {3}", employees[0], employees[1], employees[2], employees[3]);
 
         }
     }
