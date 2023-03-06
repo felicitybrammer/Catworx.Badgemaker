a278d4f76d76a7394e50a3ddd7942bcccb91af1c
Felicity Brammer <felicitybrammer@gmail.com>
2023-03-06 10:05:53 -0800

    Revert "Revert "creating methods to get and print lists""
    This reverts commit f1057872dc812a4bd9ea9673ec2bdb5e3523d912.

--- CatWorx.Badgemaker/Program.cs
+++ CatWorx.Badgemaker/Program.cs
@@ -6,77 +6,35 @@ namespace CatWorx.Badgemaker
 {
     class Program 
     {
-        static void Main(string[] args)
+         static List<string> GetEmployees()
         {
-            //Console.WriteLine("Hello, World!");
-            string greeting = "Hello";
-            greeting = greeting + " World";
-            //Console.WriteLine("greeting: " + greeting);
-            //Console.WriteLine($"greeting: {greeting}");
-            //Console.WriteLine("greeting: {0}", greeting);
+            List<string> employees = new List<string>();
+            //collect user values until the value is an empty string
+            while(true) 
+            {
+                Console.WriteLine("Please enter a name: (leave empty to exit): ");
+                string input = Console.ReadLine();
+                //break if user hits ENTER without typing a name
+                if (input == "") 
+                {
+                    break;
+                }
+                employees.Add(input);
+            }
+            return employees;
+        }
 
-            // How do you find the area of a square? Area = side * side
-            double side = 3.14;
-
-            double area = side * side;
-            //Console.WriteLine("Area: {0}", area);
-            //Console.WriteLine("Area is a {0}", area.GetType());
-
-            // Console.WriteLine(2 * 3);
-            // Console.WriteLine(10 % 3);        // Modulus Op => remainder of 10/3
-            // Console.WriteLine(1 + 2 * 3);     // order of operations
-            // Console.WriteLine(10 / 3.0);      // int's and doubles
-            // Console.WriteLine(10 / 3);        // int's 
-            // Console.WriteLine("12" + "3");    // What happens here?
-            // int num = 10;
-            // num += 100;
-            // Console.WriteLine(num);
-            // num ++;
-            // Console.WriteLine(num);
-            // bool isCold = true;
-            // Console.WriteLine(isCold ? "drink" : "add ice");  // output: drink
-            // Console.WriteLine(!isCold ? "drink" : "add ice");  // output: add ice
-            
-            // string stringNum = "2";
-            // int intNum = Convert.ToInt16(stringNum);
-            // Console.WriteLine(intNum);
-            // Console.WriteLine(intNum.GetType());
-
-            // Dictionary<string, int> myScoreBoard = new Dictionary<string, int>(){
-            //     { "firstInning", 10 },
-            //     { "secondInning", 20 },
-            //     { "thirdInning", 30 },
-            //     { "fourthInning", 40 },
-            //     { "fifthInning", 50 },
-            // };
-            // Console.WriteLine("----------------");
-            // Console.WriteLine("  Scoreboard");
-            // Console.WriteLine("----------------");
-            // Console.WriteLine("Inning |  Score");
-            // Console.WriteLine("   1   |    {0}", myScoreBoard["firstInning"]);
-            // Console.WriteLine("   2   |    {0}", myScoreBoard["secondInning"]);
-            // Console.WriteLine("   3   |    {0}", myScoreBoard["thirdInning"]);
-            // Console.WriteLine("   4   |    {0}", myScoreBoard["fourthInning"]);
-            // Console.WriteLine("   5   |    {0}", myScoreBoard["fifthInning"]);
-
-            // string[] favFoods = new string[3]{ "pizza", "doughnuts", "icecream"};
-            // string firstFood = favFoods[0];
-            // string secondFood = favFoods[1];
-            // string thirdFood = favFoods[2];
-            // Console.WriteLine("I like {0}, {1}, and {2}", firstFood, secondFood, thirdFood);
-
-            List<string> employees = new List<string>() { "adam", "amy"};
-            employees.Add("barbara");
-            employees.Add("billy");
-            // Console.WriteLine("My employees include {0}, {1}, {2}, {3}", employees[0], employees[1], employees[2], employees[3]);
-            Console.WriteLine("Please enter a name: ");
-
-            string input = Console.ReadLine();
-            employees.Add(input);
-
+        static void PrintEmployees(List<string> employees)
+        {
             for (int i = 0; i < employees.Count; i++) {
                 Console.WriteLine(employees[i]);
             }
+        }
+
+        static void Main(string[] args)
+        {
+            List<string> employees = GetEmployees();
+            PrintEmployees(employees);
         }
     }
 }
